fab455eefe4c1f741494ee1105af2b9949d006c4
HelenArkh <alena.arkh@gmail.com>
2021-01-23 19:33:33 +0300

    Конструкция Try Catch Finally

--- Module9/Module9/Program.cs
+++ Module9/Module9/Program.cs
@@ -4,10 +4,32 @@ namespace Module9
 {
     class Program
     {
+        static int Division(int a, int b)
+        {
+            return a / b;
+        }
+
+        static int Sum(int a, int b)
+        {
+            return a + b;
+        }
         static void Main(string[] args)
         {
-            Exception exception = new Exception("Собственное исключения");
-            exception.HelpLink = "www.google.ru";
+            try
+            {
+                throw new RankException("Сообщение об ошибке");
+            }
+            catch (Exception ex)
+            {
+                if (ex is RankException) Console.WriteLine(ex.GetType());
+                else Console.WriteLine("Произошла непредвиденная ошибка в приложении.");
+            }
+
+            finally
+            {
+                Console.WriteLine("Блок Finally сработал!");
+            }
+
         }
 
         
