a4fd588287146082cb64e7e9796af6f776eabc83
HelenArkh <alena.arkh@gmail.com>
2021-01-23 19:06:59 +0300

    Создание проекта и исключения

---
+++ Module9/Module9/MyException.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Module9
+{
+    public class MyException:Exception
+    {
+        public MyException()
+        { }
+
+        public MyException(string message)
+            : base(message)
+        { }
+    }
+}
---
+++ Module9/Module9/Program.cs
@@ -0,0 +1,15 @@
+﻿using System;
+
+namespace Module9
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Exception exception = new Exception("Собственное исключения");
+            exception.HelpLink = "www.google.ru";
+        }
+
+        
+    }
+}
