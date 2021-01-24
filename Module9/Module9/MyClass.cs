7013ba2f7e16b7c0f7a1a803cf7d8cae527aae8a
HelenArkh <alena.arkh@gmail.com>
2021-01-24 12:45:31 +0300

    Делегаты

---
+++ Module9/Module9/MyClass.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Module9
+{
+    public class MyClass
+    {
+        public void Process()
+        {
+            Console.WriteLine("Процесс начат");
+            Console.WriteLine("Процесс окончен");
+        }
+    }
+}
--- Module9/Module9/Program.cs
+++ Module9/Module9/Program.cs
@@ -9,29 +9,89 @@ namespace Module9
             return a / b;
         }
 
-        static int Sum(int a, int b)
+        static void Sum1(int a, int b)
         {
-            return a + b;
+            Console.WriteLine(a + b);
         }
+
+        static void Subtraction (int a, int b)
+        {
+            Console.WriteLine(b - a);
+        }
+
+
+        delegate int SumDelegate(int a, int b, int c);
+        delegate bool CheckLengthDelegate(string _row);
+
+        public delegate string GreetingsDelegate(string name);
+
+        delegate void ShowMessageDelegate(string _message);
+
+        delegate int RandomNumberDelegate();
+
+        delegate int Calculate(int a, int c);
+
         static void Main(string[] args)
         {
-            try
+            RandomNumberDelegate randomNumberDelegate = () =>
             {
-                throw new RankException("Сообщение об ошибке");
-            }
-            catch (Exception ex)
-            {
-                if (ex is RankException) Console.WriteLine(ex.GetType());
-                else Console.WriteLine("Произошла непредвиденная ошибка в приложении.");
-            }
-
-            finally
-            {
-                Console.WriteLine("Блок Finally сработал!");
-            }
+                return new Random().Next(0, 100);
+            };
+            int result = randomNumberDelegate.Invoke();
+            Console.WriteLine(result);
+            Console.Read();
 
         }
 
-        
+        static int Sum(int a, int b, int c)
+        {
+            return a + b + c;
+        }
+
+        static bool CheckLength(string _row)
+        {
+            if (_row.Length > 3) return true;
+            return false;
+        }
+
+        private static bool IsApple(string modelName)
+        {
+            if (modelName == "IPhone X") return true;
+            else return false;
+        }
+
+        static void Display(string message)
+        {
+            Console.WriteLine(message);
+        }
+
+        private static int AddNumbers(int param1, int param2)
+        {
+            return param1 + param2;
+        }
+
+        static void ShowMessage1()
+        {
+            Console.WriteLine("Метод 1");
+        }
+
+        static void ShowMessage2()
+        {
+            Console.WriteLine("Метод 2");
+        }
+
+        static void ShowMessage3()
+        {
+            Console.WriteLine("Метод 3");
+        }
+
+        static void ShowMessage4()
+        {
+            Console.WriteLine("Метод 4");
+        }
+
+       
+
+
     }
 }
